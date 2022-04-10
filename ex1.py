82e5ffc88f5ca14377b1b8bb29849cd4a7292b38
shirabron <bronstein3@mail.tau.ac.il>
2022-04-10 20:36:05 +0300

    Update ex1.py

--- ex1.py
+++ ex1.py
@@ -17,16 +17,18 @@ def main(K,max_iter,input,output):
     input = input()
     output = input()
     vectors = readInput(input)
+    if validityOfK(K,len(vectors))==False:
+        print("Invalid Input!")
     centroids = intializeCentroids(vectors,K)
 
     for i in range(max_iter):
         clusters = []
         num_of_unchanged_centorids = 0
         for i in range(len(vectors)):
-            assignVectorToCluster(centroids,clusters,i)
+            assignVectorToCluster(vectors,centroids,clusters,i)
         for i in range(K):
             old_centroid = centroids[i]
-            updateCentroid(i)
+            updateCentroid(vectors,centroids,clusters,i)
             new_centroid = centroids[i]
             if euclideanDistance(old_centroid,new_centroid)<0.001:
                 num_of_unchanged_centorids+=1
@@ -36,12 +38,11 @@ def main(K,max_iter,input,output):
 
     
             
-# !!!!!!!! correct this!
+# checking in the size of k is valid
 def validityOfK(k,n):
     if 1<k and k<n:
-        return
-    print("Invalid Input!")
-    return 
+        return True
+    return False
 
 # func to read data from file, return a list of lstsc(vectors = [],vectorCooirdinates = [])
 def readInput(fileName):
@@ -74,16 +75,28 @@ def euclideanDistance(x,y):
     return math.sqrt(sumOfSquaredDiffrence)
 
 # updates and returns centroid
-def updateCentroid(n,centroid,vector):
-
-    return True
+def updateCentroid(vectors,centroids,clusters,i):
+    centroid=centroids[i]
+    cluster=clusters[i]
+    size = len(cluster)
+    for j in range(len(centroid)):
+        sum = 0
+        for index in cluster:
+            sum+=vectors[index][j]
+        centroid[j]=sum/size
+    return
 
 
 # assigns vector to closest cluster
-def assignVectorToCluster(centroids,clusters,vectorIndex):
-    return
-
-def updateCentroid(centroids,clusters):
+def assignVectorToCluster(vectors,centroids,clusters,vectorIndex):
+    min = sys.float_info.max
+    index = 0
+    for i in range (len(centroids)):
+        distance = euclideanDistance(vectors[vectorIndex],clusters[i])
+        if distance<min:
+            min = distance
+            index = i
+    clusters[index].append(vectorIndex)
     return
 
 def writeToOutput(fileName,centroids):
